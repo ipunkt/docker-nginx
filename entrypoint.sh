ae28ceca0b7129bd0c32f53eed27d2abb18d445d
Sven Speckmaier <sps@ipunkt.biz>
2017-12-12 12:47:35 +0100

    removed waiting for db added legacy support for files in `/etc/nginx/conf.template.d`

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,27 +41,16 @@ if [ ! -z "$CACHE_DIRECTORY" ] ; then
 	chmod -R 770 "$CACHE_DIRECTORY"
 fi
 
-# Check MySQL Connection
-if [ "$DB_HOST" = "" -o "$DB_USERNAME" = "" -o "$DB_PASSWORD" = "" ]
-then
-	echo "DB_HOST, DB_USERNAME or DB_PASSWORD not set, continueing without waiting for database."
-	echo "Please set DB_HOST DB_USERNAME and DB_PASSWORD to enable database wait"
-else
-	echo "Waiting for Database connection on $DB_HOST"
-	mysql -h $DB_HOST -u $DB_USERNAME "--password=$DB_PASSWORD" -e exit
-	while [ "$?" != "0" ]
-	do echo "Waiting 5s until next try to"
-		sleep 5s
-		mysql -h $DB_HOST -u $DB_USERNAME "--password=$DB_PASSWORD" -e exit
-	done
-fi
-
 # This command causes the script to fail if any command exits with a status != 0
 # In effect if any command fails then the container will stop - a visual
 # indication that something has gone wrong
 set -e
 
-/usr/local/bin/confd -onetime -backend env
+if [ -d /etc/nginx/conf.template.d ] ; then
+	cp -R /etc/nginx/conf.template.d/* /etc/nginx/conf.d/
+else
+	/usr/local/bin/confd -onetime -backend env
+fi
 
 echo Starting NGINX
 nginx -g "daemon off;"
